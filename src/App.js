48ebd776534fbd51c0edf0616f860aabfa5f42eb
Hocine.Alili <Hocine.Alili@traceone.com>
2022-05-21 23:36:45 +0200

    Final commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Tooltip from "./Tooltip/Tooltip";
+import "./App.css";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Try hovering elements in page</h1>
+
+      <div className="example-wrapper top-left">
+        <Tooltip content="Some Text">
+          <label>Label</label>
+          <input type="text"/>
+        </Tooltip>
+      </div>
+
+      <div className="example-wrapper top-right">
+        <Tooltip content="Some other text!">
+          <p>
+            Lorem ipsum dolor sit amet
+          </p>
+        </Tooltip>
+      </div>
+
+      <div className="example-wrapper">
+        <Tooltip content={<> <button>Hello</button></> } delay={1000}>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. A aliquam consectetur id incidunt porro quam quis,
+            repudiandae saepe sapiente tempore. Atque dolorem eaque fugit ipsa ipsam libero maiores nam repudiandae!
+          </p>
+        </Tooltip>
+      </div>
+
+      <div className="example-wrapper bottom-left">
+        <Tooltip content="Some Text">
+          <button>Hover me</button>
+        </Tooltip>
+      </div>
+
+      <div className="example-wrapper bottom-right">
+        <Tooltip content="Some Text">
+          Bottom Right
+        </Tooltip>
+      </div>
     </div>
   );
 }
-
-export default App;
---
+++ src/Tooltip/Tooltip.js
@@ -0,0 +1,75 @@
+import React, {useRef, useState} from "react";
+import "./Tooltip.css";
+
+const Tooltip = (props) => {
+  let timeout;
+  const [visible, setVisible] = useState(false);
+  const [position, setPosition] = useState(null);
+
+  const showTip = (el) => {
+    timeout = setTimeout(() => {
+      setVisible(true)
+      showPositioned(el);
+    }, props.delay || 500);
+  };
+
+  const showPositioned = (el) => {
+    let elem = el.currentTarget;
+    let hoverRect = elem.getBoundingClientRect();
+
+    if (tooltipRef.current) {
+      const docWidth = document.documentElement.clientWidth;
+      const docHeight = document.documentElement.clientHeight;
+
+      let mostRightX = hoverRect.x + hoverRect.width;
+      let mostLeftX = hoverRect.x;
+      let rightY = hoverRect.y;
+      let bottomY = hoverRect.y + hoverRect.height;
+
+      let tooltipWidth = tooltipRef.current.clientWidth;
+      let tooltipHeight = tooltipRef.current.clientHeight;
+
+      let allowRight = (mostRightX + tooltipWidth) <= (window.scrollX + docWidth);
+      let allowLeft = (mostLeftX - tooltipWidth) >= 0;
+
+      let allowTop = (rightY - tooltipHeight) >= 0;
+      let allowBottom = (bottomY + tooltipHeight) <= (window.scrollY + docHeight);
+
+      let newPosition = null
+
+      if (allowRight) {
+        newPosition = "right";
+      } else if (allowBottom) {
+        newPosition = "bottom";
+      } else if (allowLeft) {
+        newPosition = "left";
+      } else if (allowTop) {
+        newPosition = "top";
+      }
+      setPosition(newPosition);
+    }
+  }
+
+  const hideTip = () => {
+    clearInterval(timeout);
+    setVisible(false);
+  };
+
+  return (
+    <div
+      className="Tooltip-Wrapper"
+      onMouseEnter={showTip}
+      onMouseLeave={hideTip}
+    >
+      {props.children}
+      <div
+        className={`Tooltip-Tip ${visible ? 'visible' : 'visibility_hidden'} ${position}`}
+        ref={tooltipRef}
+      >
+        {props.content}
+      </div>
+    </div>
+  );
+};
+
+export default Tooltip;
